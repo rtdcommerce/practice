2ca1687a18eb2813e473e00da075f97225c905d9
rtdcommerce <Duncan@Duncans-MacBook-Pro.local>
2016-07-23 22:15:01 -0400

    Added Comments to code

--- swift.playground/Contents.swift
+++ swift.playground/Contents.swift
@@ -2,10 +2,11 @@
 
 import UIKit
 
-
+// Variables
 var reinsPiggyBank = 200.00
 var newLegoGame = 59.99
 
+// Passing Parameters Into A Function
 func purchaseGame(reinsPiggybank: Double, newLegoGame: Double) -> Double {
     return reinsPiggybank - newLegoGame
 }
@@ -15,7 +16,7 @@ if reinsPiggyBank >= newLegoGame {
 }else {
     print("I Didnt Have Enough money!")
 }
-
+// Calling a Function
 purchaseGame(200.00, newLegoGame: 59.99)
 
 
